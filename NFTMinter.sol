2f68f3a8bc8dd4a8ede93e06bab27b8380912d46
coinResearch2021 <89898817+coinResearch2021@users.noreply.github.com>
2021-09-03 11:52:01 +0900

    Update NFTMinter.sol

--- NFTMinter.sol
+++ NFTMinter.sol
@@ -7,7 +7,7 @@ contract NFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("GameItem", "ITM") {}
+    constructor() ERC721("CoinResearch", "CRC") {}
 
     mapping(uint => string) tokenURIs;
 
@@ -15,7 +15,7 @@ contract NFT is ERC721 {
       return tokenURIs[tokenId];
     }
 
-    function create(address player, string memory tokenURI)
+    function create(address to, string memory tokenURI)
         public
         returns (uint256)
     {
