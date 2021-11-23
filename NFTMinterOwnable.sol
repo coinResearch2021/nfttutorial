f0dffbf3bf28aafc9b191d6007ffe10cf5f37723
coinResearch2021 <89898817+coinResearch2021@users.noreply.github.com>
2021-11-23 20:48:09 +0900

    Add files via upload

---
+++ NFTMinterOwnable.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract BibimbeatTestNFTNoOwner is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Bibimbeat Test NFT", "BBT") {}
+
+    mapping(uint => string) tokenURIs;
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+      return tokenURIs[tokenId];
+    }
+
+    function create(address to, string memory tokenURI) public onlyOwner returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(to, newItemId);
+        tokenURIs[newItemId] = tokenURI;
+
+        return newItemId;
+    }
+}
